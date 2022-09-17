ac1f8afd0826369cb8ef1c6af40f1a42ef64c8ed
SaeemaChanda02 <113799538+SaeemaChanda02@users.noreply.github.com>
2022-09-18 02:28:14 +0530

    Add files via upload

---
+++ Annotations.java
@@ -0,0 +1,33 @@
+// Java Program to Demonstrate Type Annotation
+
+// Importing required classes
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+// Using target annotation to annotate a type
+//@Target(ElementType.TYPE_USE)
+
+// Declaring a simple type annotation
+//@interface TypeAnnoDemo{}
+
+// Main class
+public class Annotations {
+
+	// Main driver method
+	public static void main(String[] args) {
+
+		// Annotating the type of a string
+		 String string = "I am annotated with a type annotation";
+		System.out.println(string);
+		abc();
+	}
+
+	// Annotating return type of a function
+	static  int abc() {
+	
+		System.out.println("This function's return type is annotated");
+	
+		return 0;
+	}
+}
+
