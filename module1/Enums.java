5bc64ed3a642b6631289dbdf8c25609033e02906
SaeemaChanda <113799538+SaeemaChanda02@users.noreply.github.com>
2022-09-19 15:43:36 +0530

    Add files via upload

---
+++ module1/Enums.java
@@ -0,0 +1,19 @@
+// enum declaration inside a class.
+
+public class Enums {
+ enum Color {
+  RED,
+  GREEN,
+  BLUE;
+ }
+
+ // Driver method
+ public static void main(String[] args)
+ {
+  Color c1 = Color.RED;
+  System.out.println(c1);
+ }
+}
+
+
+
