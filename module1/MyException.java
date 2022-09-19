bcd338aaf2b3b9e1333c640db4662bf7033de3dd
SaeemaChanda <113799538+SaeemaChanda02@users.noreply.github.com>
2022-09-19 15:44:13 +0530

    Add files via upload

---
+++ module1/MyException.java
@@ -0,0 +1,18 @@
+
+public class MyException {
+
+	//Java Program to Demonstrate How Exception Is Thrown
+	 
+	 public static void main(String args[])
+	 {
+	  // Taking an empty string
+	  String str = null;
+	  System.out.println(str.length()); // to find length of string which is null
+	 }
+	}
+
+
+	 
+    
+    
+    
